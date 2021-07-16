43c2a7f9eb9f2d4ac0b1878a099bfdfe48bb7d91
mekalatharun <85494416+mekalatharun@users.noreply.github.com>
2021-07-16 18:07:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var superheroes=require("superheroes");
+var mysuperheroName=superheroes.random();
+console.log(mysuperheroName);
+var supervillains=require("supervillains");
+var myVillain=supervillains.random();
+console.log(myVillain);
